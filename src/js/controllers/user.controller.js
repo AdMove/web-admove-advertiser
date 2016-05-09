d787134c0c9db698a2e32392450c509a9b7c2d5c
Giorgi <giorgirokhadze@gmail.com>
2016-05-09 20:13:21 +0400

    Updated controller

--- src/js/controllers/user.controller.js
+++ src/js/controllers/user.controller.js
@@ -1,13 +1,12 @@
 (function () {
     'use strict';
 
-    App.controller('UserController', ['$scope', 'UserService', function ($scope, UserService) {
+    App.controller('UserController', ['$scope', function ($scope) {
         var self = this;
-        self.user = { id: null, username: '', password: '', email: ''};
+        self.user = {id: null, username: '', password: '', email: ''};
 
         self.reset = function () {
             self.user = {id: null, username: '', address: '', email: ''};
-            $scope.myForm.$setPristine();
         };
 
     }]);
