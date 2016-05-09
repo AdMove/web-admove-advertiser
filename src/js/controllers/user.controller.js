1d1681106bf8656e5cebc0f75dd9a90e3024893f
Giorgi <giorgirokhadze@gmail.com>
2016-05-09 20:08:39 +0400

    Deleted unused file

--- src/js/controllers/user.controller.js
+++ src/js/controllers/user.controller.js
@@ -1,3 +1,15 @@
-/**
- * Created by Giorgi on 5/9/2016.
- */
+(function () {
+    'use strict';
+
+    App.controller('UserController', ['$scope', 'UserService', function ($scope, UserService) {
+        var self = this;
+        self.user = { id: null, username: '', password: '', email: ''};
+
+        self.reset = function () {
+            self.user = {id: null, username: '', address: '', email: ''};
+            $scope.myForm.$setPristine();
+        };
+
+    }]);
+
+})();
